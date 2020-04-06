09df74457937133514cf0dca20d30d001734a483
asirko <sirko.alexandre@gmail.com>
2020-04-06 23:20:52 +0200

    ask how distribute

--- src/app/features/belote/game/round/round.component.ts
+++ src/app/features/belote/game/round/round.component.ts
@@ -1,8 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { BeloteService } from '../../belote.service';
-import { map } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { PLAYER_ID_KEY } from '../../../../shared/pseudo/pseudo.guard';
 import { Belote } from '../../belote';
+import { PseudoComponent } from '../../../../shared/pseudo/pseudo.component';
+import { of, Subscription } from 'rxjs';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { DistributeComponent } from './distribute.component';
 
 @Component({
   selector: 'app-round',
@@ -10,7 +14,9 @@ import { Belote } from '../../belote';
   styleUrls: ['./round.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoundComponent {
+export class RoundComponent implements OnDestroy {
+  dialogDistributionSub: Subscription;
+
   game$ = this.beloteService.fireGame.valueChanges().pipe(
     map(game => {
       const currentPlayerId = localStorage.getItem(PLAYER_ID_KEY);
@@ -18,7 +24,23 @@ export class RoundComponent {
       const players = game.players.map((p, i, list) => list[(i + currentPlayerIndex) % 4]);
       return { ...game, players } as Belote;
     }),
+    tap(game => this.manageDistribution(game)),
   );
 
-  constructor(private beloteService: BeloteService) {}
+  constructor(private beloteService: BeloteService, private matDialog: MatDialog) {}
+
+  ngOnDestroy(): void {
+    this.dialogDistributionSub?.unsubscribe();
+  }
+
+  manageDistribution(game: Belote) {
+    if (game.turnTo === localStorage.getItem(PLAYER_ID_KEY) && !this.dialogDistributionSub) {
+      this.dialogDistributionSub = this.matDialog
+        .open(DistributeComponent, { width: '250px', disableClose: true })
+        .afterClosed()
+        .subscribe(v => {
+          console.log(v);
+        });
+    }
+  }
 }
